4ade093a595882438a1a91e5edca11f60eea08f4
CarolineKuhn <carolinekuhn2@hotmail.com>
2023-03-14 12:13:21 +0000

    login user/charity routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,33 +1,38 @@
-const router = require('express').Router();
-const mongoose = require('mongoose');
-
-const User = require('../models/User.model'); 
+const router = require("express").Router();
+const mongoose = require("mongoose");
+const Charity = require("../models/Charity.model");
+const User = require("../models/User.model");
 
 // User routes
 
 //Get all users
-router.get('/users', async (req, res) => {
-    try {
-      const users = await User.find();
-      res.json(users);
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  });
- 
-
-  // Get all users
-router.get("/users/:id", async (req, res) => {
-  const {id} = req.params
+router.get("/users", async (req, res) => {
   try {
-    const users = await User.findById(id);
+    const users = await User.find();
     res.json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-  // Create a new user
+// Get all users
+router.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const users = await User.findById(id);
+
+    if (!users) {
+      const charity = await Charity.findById(id);
+      res.json(charity);
+    } else {
+      res.json(users);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Create a new user
 /*   router.post('/users', async (req, res) => {
     const user = new User({
       name: req.body.name,
@@ -43,5 +48,4 @@ router.get("/users/:id", async (req, res) => {
     }
   }); */
 
-  
-  module.exports = router;
+module.exports = router;
